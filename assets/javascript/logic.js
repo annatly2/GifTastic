e84c68f2fe39a2380ba42d98363770065fe69eec
Anna Ly <annatly2@gmail.com>
2017-09-13 23:00:10 -0700

    sept 13

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,44 +1,68 @@
-var topics = ["Master of None", "Aziz Ansari", "pasta"];
+$(document).ready(function(){
+var topics = ["Master of None", "Aziz Ansari", "Alan Yang", "Lena Waithe", "New York City", "Pasta"];
 
-for(i = 0; i < topics.length; i++){
-        console.log("hello");
-        var newButton = $("<button>");
-        newButton.addClass("topics");
-        newButton.attr("gif-name", topics[i]);
-        newButton.text(topics[i]);
-        $("buttons-view").append(newButton);
-	
+function displayGifs(){
+  var topicName = $(this).attr("data-name");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+          url: queryURL,
+          method: "GET"
+  }).done(function(response){
+    console.log(response);
+    var results = response.data;
+    for (var i = 0; i < results.length; i++){
+      var gifDiv = $("<div class = 'item'>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: "+rating);
+
+      var topicImg = $("<img>");
+      topicImg.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(topicImg);
+
+      $("#gifs-appear-here").prepend(gifDiv);
+    }
+
+  });
+
+  $("#gifs-appear-here").html("");
 }
 
-$("button").on("click", function(){
 
-	var food = $(this).attr("data-food");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        food + "&api_key=dc6zaTOxFJmzC&limit=10";
+function renderButtons(){
 
-        $.ajax({
-        	url: queryURL,
-        	method: "GET"
-        })
-        .done(function(response){
-        	console.log(response);
-        	var results = response.data;
-        	for (var i = 0; i < results.length; i++){
-        		var gifDiv = $("<div class = 'item'>");
-        		var rating = results[i].rating;
-        		var p = $("<p>").text("Rating: "+ rating);
+$("#buttons-view").empty();
 
-        		var foodImage = $("<img>");
-        		foodImage.attr("src", results[i].images.fixed_height.url);
+  for(var i = 0; i < topics.length; i++){
+          console.log("hello");
+          var newButton = $("<button>");
+          newButton.addClass("topic");
+          newButton.attr("data-name", topics[i]);
+          newButton.text(topics[i]);
+          $("#buttons-view").append(newButton);
+  }
+}
 
-        		gifDiv.prepend(p);
-        		gifDiv.prepend(foodImage);
+$("#add-topic").on("click", function(event){
+  event.preventDefault();
 
-        		$("#gifs-appear-here").prepend(gifDiv);
+  var newTopic = $("#topic-input").val().trim();
+  topics.push(newTopic);
+  console.log(topics);
+  renderButtons();
 
-        	}
+});
 
+$(document).on("click", ".topic", displayGifs);
+renderButtons();
 
-        });
+$(".item").on("click", function(){
+
+results[i].images.original_still.url
+
+});
+
 
 });
